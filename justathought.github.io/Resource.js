779a93ced984ccf18cd8b6a6e6d92a36bae368f1
Ralph Huey Olegario <ralphhueyolegario@Ralphs-MacBook-Pro.local>
2021-12-15 17:07:26 +1300

    Added dynamic list and changed up algorithm for making cards show up

--- justathought.github.io/Resource.js
+++ justathought.github.io/Resource.js
@@ -31,14 +31,15 @@ input.addEventListener('keyup', (event) => {
     }
   });
 input.addEventListener('keyup', paragraphCheck);
+//checks the paragraphs of the cards
 function paragraphCheck()
 {
     var searchContainer = document.getElementById("autoComplete_list_1");
     var searchList = document.querySelectorAll('#autoComplete_list_1 > li');
     var searchLength = searchList.length;
+    var a = 0;
     if (input.value !== "" && input.value.length >= 3)
     {
-        setTimeout(600);
         for(i = 0; i < (contentParagraph.length); i++)
         {
             if (contentParagraph[i].textContent.toLowerCase().includes(input.value.toLowerCase()))
@@ -47,24 +48,55 @@ function paragraphCheck()
                     var x = document.createElement("li");
                     var t = document.createTextNode(contentTitle[i].textContent);
                     x.setAttribute("role", "option");
-                    x.setAttribute("class", contentTitle[i].textContent);
                     x.appendChild(t);
                     content[i].style.visibility = "visible";
                     content[i].style.display = "block";
+                    try
+                    {
+                    if(document.body.contains(document.querySelector('.no_result')))
+                        {
+                            document.querySelector('.no_result').style.visibility = "hidden";
+                            document.querySelector('.no_result').style.display = "none";
+                        }
+                    }
+                    catch(e)
+                    {
+                        console.log(e);
+                    }
                     //check if there is a duplicate in searchList            
                     for(j = 0; j < searchList.length; j++)
                     {
-                        if(searchList[j].className === contentTitle[i].textContent)
+                        console.log(searchList[j].textContent === contentTitle[i].textContent);
+                        if(searchList[j].textContent === contentTitle[i].textContent)
                         {
                             duplicateCheck = true;
                         }
-                    }       
+                    }
+                    //if the duplicate check isnt activated      
                     if(!duplicateCheck)
                     { 
-                        x.setAttribute(onclick, function(){var justify = this.getAttribute('class');
+                        //if the LI in question is clicked, then the following div will show up
+                        x.onclick = function(){
+                        var justify = this.textContent;
+                        input.value = justify;
                         console.log(justify);
-                    })
-                        x.setAttribute("id", "autoComplete_result_"+(1+searchLength));
+                        for(i = 0; i < content.length; i++)
+                        {
+                            if(contentTitle[i].textContent === justify)
+                            {
+                                content[i].style.visibility = "visible";
+                                content[i].style.display = "block";
+                            }
+                            else
+                            {
+                                content[i].style.visibility = "hidden";
+                                content[i].style.display = "none";
+                            }
+                        }
+                    }
+                        a++;
+                        console.log(a);
+                        x.setAttribute("id", "autoComplete_result_"+(a + searchLength));
                         searchContainer.appendChild(x);
                     }
             }
@@ -72,6 +104,7 @@ function paragraphCheck()
     }
     
 }
+//starts when the buttons are pressed
 function buttonSearching(buttonText)
 {
     var buttonWrapperButtons = document.querySelectorAll('#buttonWrapper > button');
